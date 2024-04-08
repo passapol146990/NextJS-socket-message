948a8b3b9fd7e67f5eae6a4bc5fbf23066cbcade
Passapol <phasphlsuthahthrrm976@gmail.com>
2024-04-08 15:43:56 +0700

    update 0.0.1

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { createServer } = require('http');
+const { join } = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+io.on('connection', (socket) => {
+    socket.on('hello', (arg) => {
+      console.log(arg); // 'world'
+    });
+  });
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
